7ee254c8383b6bd55204a7753f8597a271046b69
freegroup4 <freegroup4@yopmail.com>
2014-05-25 07:50:19 +0800

    First Commit
    First

---
+++ src/maxdemo/MaxDemo.java
@@ -0,0 +1,22 @@
+package maxdemo;
+public class MaxDemo {
+    public static void main(String[] args) {
+        int i,max,min;
+        int arr[] = {74,48,5,30,17,62,99};
+        System.out.println("");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
